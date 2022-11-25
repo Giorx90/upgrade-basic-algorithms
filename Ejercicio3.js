dbfcd11f1316cfecf11d839916f9d92e465b97ff
Giorx_90 <j.maldo.1990@gmail.com>
2022-11-25 21:51:54 +0100

    Iteración#3

---
+++ Ejercicio3.js
@@ -0,0 +1,16 @@
+let op1 = 10*5
+console.log (op1)
+
+let op2 = 10/2
+console.log (op2)
+
+let op3 = 15%9
+console.log (op3)
+
+let y = 10
+let z = 5
+let x = y + z
+console.log(x)
+
+x = y * z
+console.log(x)
