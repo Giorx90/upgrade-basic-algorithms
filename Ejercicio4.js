4be30ff1e91a76191ef5c6f8fbe703fd7c2575ba
Jorge M <j.maldo.1990@gmail.com>
2022-12-02 19:47:43 +0100

    Iteración#4

---
+++ Ejercicio4.js
@@ -0,0 +1,20 @@
+//const avengers = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
+//console.log(avengers[0]);
+
+//avengers.splice(0, 1, "IRONMAN");
+//console.log(avengers);
+
+//const avengers = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
+//console.log(avengers.length);
+
+//const rickAndMortyCharacters = ["Rick", "Beth", "Jerry"];
+//rickAndMortyCharacters.push("Morty", "Summer")
+//console.log(rickAndMortyCharacters[4])
+
+//const rickAndMortyCharacters = ["Rick", "Beth", "Jerry", "Morty", "Summer", "Lapiz Lopez"];
+//rickAndMortyCharacters.pop
+//console.log(rickAndMortyCharacters[0], rickAndMortyCharacters[4])
+
+//const rickAndMortyCharacters = ["Rick", "Beth", "Jerry", "Morty", "Summer", "Lapiz Lopez"];
+//rickAndMortyCharacters.splice(1,1)
+//console.log(rickAndMortyCharacters)
